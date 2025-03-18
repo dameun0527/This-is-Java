f84045606927f10ef65b4cf23751d8771d8ebe9a
dameun0527 <jisuryu0527@gmail.com>
2025-03-18 18:29:28 +0900

    git init

---
+++ src/main/java/com/example/this_is_java/ThisIsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.this_is_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisIsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisIsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/this_is_java/ThisIsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.this_is_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisIsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
